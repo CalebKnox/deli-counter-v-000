e9dea5b8d1a3b863808bf0eb7b5a81cab94893ea
Caleb Knox <knoxcaleb@aol.com>
2016-06-03 18:24:26 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,28 @@
 # Write your code here.
 katz_deli = []
-$phrase = "The line is currently:"
-def line(names)
-  if names == []
+
+def line(katz_deli)
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    names.each_with_index do |name, index|
-      $phrase += " #{index + 1}. #{name}"
+    phrase = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      phrase += " #{index + 1}. #{name}"
     end
-    puts $phrase
+    puts phrase
   end
 end
 
-def take_a_number(names, name)
-  line = names
-  line << name
-  puts "Welcome, #{name}. You are number #{line.size} in line."
-  return line
+def take_a_number(deli, customer)
+  deli << customer
+  puts "Welcome, #{customer}. You are number #{deli.length} in line."
 end
 
-def now_serving(names)
-  if names == []
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{names[0]}."
-  return names.shift
+    puts "Currently serving #{deli.first}."
+    deli.shift
   end
 end
